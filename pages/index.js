9a8646d524e29b45eb6920ab4810e2b0f2a9b158
Victor Hugo Turcato <144856749+vhturcato@users.noreply.github.com>
2024-07-06 14:48:31 +0000

    testa links 3

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 function Home (){
-    return <h1>agora vai 2 😎 </h1>
+    return (
+    
+    <h1>
+    Page2 <Link href="/page2.js">Page 2</Link>
+    </h1>
+    )
 }
-
-<h1>
-  Page2 <Link href="/page2.js">Page 2</Link>
-</h1>
-
 
 export default Home;
