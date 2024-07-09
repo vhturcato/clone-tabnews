03c879cf9c65b022b62b8afa5603478917df0af1
Victor Hugo Turcato <144856749+vhturcato@users.noreply.github.com>
2024-07-09 21:41:10 +0000

    tinha mudancas

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,9 @@ import Link from "next/link";
 function Home() {
   return (
     <h1>
-      Page2 ae <Link href="/page2">Page 2</Link>
+      Link para <Link href="/page2">Page 2</Link>
     </h1>
   );
-}
-
-function teste2() {
-  console.log("indentação errada");
 }
 
 export default Home;
--- pages/page2.js
+++ pages/page2.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function p2() {
   return (
     <h1>
-      Index <Link href="/">Link to page 2</Link>
+      Link para <Link href="/">Index</Link>
     </h1>
   );
 }
